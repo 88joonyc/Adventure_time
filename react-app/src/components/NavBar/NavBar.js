e30bb463df1259512d1f426dbf34633a6c0fc649
88joonyc <joonyc88@gmail.com>
2021-08-24 19:21:57 -0400

    navbar logo;

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -4,42 +4,42 @@ import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { useSelector } from 'react-redux';
 
+import './NavBar.css'
+
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user)
 
 
   let content = null
 
-  if (sessionUser) {
+  if (!sessionUser) {
     content = (
       <nav>
-
-      <ul className="navbar-unordered">
-        <li>
-
+      <div className="navbar-container">
+        <div>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            <img className="navbar-image logo-image-container"/>
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <LogoutButton />
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
     )
   }
