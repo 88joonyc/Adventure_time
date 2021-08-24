24a87b27fa437a595cd10dd6be1a18436d8c0fc7
88joonyc <joonyc88@gmail.com>
2021-08-24 16:36:30 -0400

    moved sing up form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginPage/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
+import SignUpForm from './components/auth/SignupPage/SignUpForm';
 import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
