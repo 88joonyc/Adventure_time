90a88ca3b81da521ddab0fa054c03ee0d3a6d9df
88joonyc <joonyc88@gmail.com>
2021-08-25 11:01:45 -0400

    fixed verification buttons

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -14,42 +14,53 @@ const NavBar = () => {
 
   let content = null
 
+  let verificationtabs = (
+
+    <>
+
+              <div className='login-signup'>
+                <NavLink className="signup-link" to='/login' exact={true} activeClassName='active'>
+                  Log In
+                </NavLink>
+              </div>
+              <div className='login-signup'>
+                <NavLink className="signup-link" to='/sign-up' exact={true} activeClassName='active'>
+                  Sign Up
+                </NavLink>
+              </div>
+
+    </>
+  )
+
   if (searchBar) {
     content = (
     <>
-    <div>
+    <div className="searchbar-container">
       <input
         placeholder='Search events'
         className='search-input'
       />
-    <button className='cancel-button' onClick={() => toggleSearch(!searchBar)}>cancel</button>
+      <button className='cancel-button' onClick={() => toggleSearch(!searchBar)}>cancel</button>
     </div>
     </>
     )
   } else {
-        content = (
-          <nav>
+      content = (
+        <nav>
           <div className="navbar-container">
             <div>
               <NavLink to='/' exact={true} activeClassName='active'>
                 <img className="navbar-image logo-image-container"/>
               </NavLink>
             </div>
-            <button className="searchbar-toggle" onClick={() => toggleSearch(!searchBar)}>Search Bar</button>
-            <div className='login-signup'>
-              <NavLink to='/login' exact={true} activeClassName='active'>
-                Log In
-              </NavLink>
+                <div className="right-side-control">
+              <button className="searchbar-toggle" onClick={() => toggleSearch(!searchBar)}>
+                Search
+              </button>
+              {sessionUser ? <LogoutButton /> : verificationtabs}
             </div>
-            <div className='login-signup'>
-              <NavLink className="signup-link" to='/sign-up' exact={true} activeClassName='active'>
-                Sign Up
-              </NavLink>
-            </div>
-            {sessionUser ?
-            <LogoutButton /> : null}
           </div>
-        </nav>
+      </nav>
     )
   }
 
