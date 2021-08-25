78f9647a0a2e4ee1e40a07d47fd7d80dbfbb7d24
88joonyc <joonyc88@gmail.com>
2021-08-24 20:10:30 -0400

    break

--- react-app/src/components/auth/HomePage/SplashPage.js
+++ react-app/src/components/auth/HomePage/SplashPage.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
-
+import './SplashPage.css'
 
 const SplashPage = () => {
-    return null
+    return (
+    <>
+    <img className='spalsh-image' />
+    <div>
+        <h3>
+            Re-open confidently with Eventbrite’s COVID-19 Safety Playbook
+        </h3>
+        <h4>
+            We partnered with risk management and health experts to empower event creators to thoughtfully consider potential safety and security risks at your event. See the playbook.
+        </h4>
+    </div>
+    </>
+    )
 }
 
 
