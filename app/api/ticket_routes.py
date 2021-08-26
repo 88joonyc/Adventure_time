bab0ce7c356ec179db37d8ec0adda6e31b730254
88joonyc <joonyc88@gmail.com>
2021-08-26 18:56:50 -0400

    api routes

---
+++ app/api/ticket_routes.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import db, Event, Venue, Category, User
+from app.forms import EventForm
+
+
+ticket_routes = Blueprint('tickets', __name__)
+
+# @ticket_routes.route('/')
+# def evented():
+#     events_query = Event.query.all()
+#     venues = Venue.query.all()
+#     events = [ event.to_dict() for event in events_query ]
+#     for event in events:
+#         event['venue'] = Venue.query.get(event["venue_id"]).to_dict()
+#         event['category'] = Category.query.get(event["category_id"]).to_dict()
+#         event['user'] = User.query.get(event["host_id"]).to_dict()
+#         # event['categories'] = Category.query.all()
+#     return {'events': events}
+
+@ticket_routes.route('/')
+def event(id):
+    user = Event.query.get(id)
+    return user.to_dict()
+
+
+@ticket_routes.route('/', methods=['POST'])
+@login_required
+def create():
+    form = EventForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit:
+        event = Event(
+            host_id = current_user.id,
+            venue_id = form.data['venue_id'],
+            category_id = form.data['category_id'],
+            name = form.data['name'],
+            description = form.data['description'],
+            start_time = form.data['start_time'],
+            end_time = form.data['end_time'],
+            capacity = form.data['capacity'],
+            image = form.data['image'],
+            cost = form.data['cost'],
+        )
+        db.session.add(event)
+        db.session.commit()
+        return event.to_dict()
+
+@ticket_routes.route('/remove/<int:id>', methods=['DELETE'])
+@login_required
+def rubbish(id):
+    event = Event.query.get(id)
+    db.session.delete(event)
+    db.session.commit()
+    return event.to_dict()
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .category import Category
 from .venue import Venue
 from .event import Event
+from .ticket import Ticket
