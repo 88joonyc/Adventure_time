66d12ba923d31c6f50e3b08443e4718584ddfb1e
88joonyc <joonyc88@gmail.com>
2021-08-26 19:00:27 -0400

    migrate

--- app/api/ticket_routes.py
+++ app/api/ticket_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import db, Event, Venue, Category, User
-from app.forms import EventForm
+from app.models import db, Ticket
+from app.forms import TicketForm
 
 
 ticket_routes = Blueprint('tickets', __name__)
@@ -18,38 +18,39 @@ ticket_routes = Blueprint('tickets', __name__)
 #         # event['categories'] = Category.query.all()
 #     return {'events': events}
 
+
 @ticket_routes.route('/')
-def event(id):
-    user = Event.query.get(id)
-    return user.to_dict()
+def ticket(id):
+    tickets = Ticket.query.all()
+    return {'tickets': [ticket.to_dict() for ticket in tickets]}
 
 
-@ticket_routes.route('/', methods=['POST'])
-@login_required
-def create():
-    form = EventForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit:
-        event = Event(
-            host_id = current_user.id,
-            venue_id = form.data['venue_id'],
-            category_id = form.data['category_id'],
-            name = form.data['name'],
-            description = form.data['description'],
-            start_time = form.data['start_time'],
-            end_time = form.data['end_time'],
-            capacity = form.data['capacity'],
-            image = form.data['image'],
-            cost = form.data['cost'],
-        )
-        db.session.add(event)
-        db.session.commit()
-        return event.to_dict()
+# @ticket_routes.route('/', methods=['POST'])
+# @login_required
+# def create():
+#     form = EventForm()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     if form.validate_on_submit:
+#         event = Event(
+#             host_id = current_user.id,
+#             venue_id = form.data['venue_id'],
+#             category_id = form.data['category_id'],
+#             name = form.data['name'],
+#             description = form.data['description'],
+#             start_time = form.data['start_time'],
+#             end_time = form.data['end_time'],
+#             capacity = form.data['capacity'],
+#             image = form.data['image'],
+#             cost = form.data['cost'],
+#         )
+#         db.session.add(event)
+#         db.session.commit()
+#         return event.to_dict()
 
-@ticket_routes.route('/remove/<int:id>', methods=['DELETE'])
-@login_required
-def rubbish(id):
-    event = Event.query.get(id)
-    db.session.delete(event)
-    db.session.commit()
-    return event.to_dict()
+# @ticket_routes.route('/remove/<int:id>', methods=['DELETE'])
+# @login_required
+# def rubbish(id):
+#     event = Event.query.get(id)
+#     db.session.delete(event)
+#     db.session.commit()
+#     return event.to_dict()
--- app/models/ticket.py
+++ app/models/ticket.py
@@ -7,7 +7,7 @@ class Ticket(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id') nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id') nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
