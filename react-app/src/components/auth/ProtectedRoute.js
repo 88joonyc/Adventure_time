83d8081a3bcea3b7cc8411e29ce897a6a07d7aaa
88joonyc <joonyc88@gmail.com>
2021-08-26 20:51:55 -0400

    tickets page

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 
 const ProtectedRoute = props => {
   const user = useSelector(state => state.session.user)
   return (
     <Route {...props}>
+      <NavBar/>
       {(user)? props.children  : <Redirect to='/login' />}
     </Route>
   )
