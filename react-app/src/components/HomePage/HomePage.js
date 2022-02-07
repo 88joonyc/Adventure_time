0ea815d31cb86193cfe1fc6bcd89bd4b474ad4a7
88joonyc <joonyc88@gmail.com>
2022-02-07 18:15:05 -0500

    opening_message

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -13,6 +13,7 @@ import { authenticate } from '../../store/session';
 import EditForm from '../EditForm/EditForm';
 import PopularBar from '../PopularBar/PopularBar';
 import Search from '../Search/Search';
+import OpeningMessage from '../OpeningMessage/OpeningMessage';
 
 
 const HomePage = () => {
@@ -87,23 +88,6 @@ const handleDelete = async (e) => {
 /* --------------------------opening message------------ ------------------------------------------- */
 
 
-const opening = (
-    <>
-        <div className='spalsh-image-2'>
-            <p className="top-sent top-home-sent">Get down</p>
-            <p className="bottom-sent top-home-sent">for the HOOK!</p>
-            <button onClick={() => window.scrollTo({left: 0, top: 580, behavior: 'smooth'})} className="event-button">Find your next event</button>
-        </div>
-        <div className='opening-message'>
-            <h3 className='covid-header-title'>
-                Re-open confidently with Adventure Time's COVID-19 Safety Playbook
-            </h3>
-            <p className='covid-under-msg'>
-                We partnered with risk management and health experts to empower event creators to thoughtfully consider potential safety and security risks at your event.
-            </p>
-        </div>
-    </>
-)
 
 /* -------------------------- hearts------------ ------------------------------------------- */
 
@@ -213,7 +197,7 @@ if (!sessionUser) {
 
     return (
         <>
-        {sessionUser? opening : null}
+        {sessionUser? <OpeningMessage /> : null}
         <Search events={events} search={search} setSearch={setSearch}/>
         {content}
         <PopularBar setSearch={setSearch} sessionUser={sessionUser}/>
---
+++ react-app/src/components/OpeningMessage/OpeningMessage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OpeningMessage = () => {
+    return (
+    <>
+        <div className='spalsh-image-2'>
+            <p className="top-sent top-home-sent">Get down</p>
+            <p className="bottom-sent top-home-sent">for the HOOK!</p>
+            <button onClick={() => window.scrollTo({left: 0, top: 580, behavior: 'smooth'})} className="event-button">Find your next event</button>
+        </div>
+        <div className='opening-message'>
+            <h3 className='covid-header-title'>
+                Re-open confidently with Adventure Time's COVID-19 Safety Playbook
+            </h3>
+            <p className='covid-under-msg'>
+                We partnered with risk management and health experts to empower event creators to thoughtfully consider potential safety and security risks at your event.
+            </p>
+        </div>
+    </>
+    )
+}
+
+export default OpeningMessage
