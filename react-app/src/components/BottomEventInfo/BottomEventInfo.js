5e0cc2b089639d134d6cc0dcee5c828ec6cfe013
88joonyc <joonyc88@gmail.com>
2022-03-01 12:48:42 -0500

    bottomeventinfo

---
+++ react-app/src/components/BottomEventInfo/BottomEventInfo.js
@@ -0,0 +1,60 @@
+import React from "react";
+import moment from "moment";
+
+const BottomEventInfo = ({event}) => {
+
+    return (
+        <div className='event-page-bot-grid'>
+              <div className='events-main-cont-container'>
+                <div className='events-main-cont'>
+                  <div class='events-description-text'>
+                    <p>
+                      {moment(event?.events[0]?.start_time).format('MMM do')}
+                      - SINGLE DAY PASS - One (1) general admission RSVP to attend&nbsp;
+                    {event?.events[0]?.name} in {event?.events[0]?.venue?.city},&nbsp;
+                    {event?.events[0]?.venue?.state}
+                    </p>
+                    <h2 className="events-description-label">About this event</h2>
+                    <p className="events-description-innertext">{event?.events[0]?.description}</p>
+                    <div className="tags">Tags</div>
+                  </div>
+                </div>
+              </div>
+              <div className='side-panel-container'>
+                <div className='event-page-sidepanel'>
+                    <div className="events-info-label">
+                      <p>Capacity:</p>
+                    </div>
+                    <div className='events-address-para'>
+                      <p>{event?.events[0]?.capacity}</p>
+                    </div>
+                    <div className="events-info-label">
+                      <p>Category:</p>
+                    </div>
+                    <div className='events-address-para'>
+                      <p>{event?.events[0]?.category?.type}</p>
+                    </div>
+                    <p>Date and time</p>
+                    <p>Starts: </p>
+                    <p className='startend-times'>{moment(event?.events[0]?.start_time).add(5, 'hours').format('ddd[,] MMMM Do [,] YYYY [at] h:mm A')}</p>
+                    <p>Ends:</p>
+                    <p className='startend-times'>{moment(event?.events[0]?.end_time).add(5, 'hours').format('dddd[,] MMMM Do[,] YYYY [at] h:mm A')}</p>
+
+                    <p className="events-address-label">Location:</p>
+                    <div className='events-page-location'>
+                      <p className="events-address-para">{event?.events[0]?.venue?.name}</p>
+                      <p className="events-address-para">{event?.events[0]?.venue?.address}</p>
+                      <p className="events-address-para">{event?.events[0]?.venue?.city}, {event?.events[0]?.venue?.state}</p>
+                      <p className="events-address-para">{event?.events[0]?.venue?.zip_code}</p>
+                      <p className="events-address-para">Lat: {event?.events[0]?.venue?.latitude} Lon: {event?.events[0]?.venue?.longitude} </p>
+                      <button className='view-map-button' onClick={() => document.querySelector('.the-real-map').scrollIntoView({behavior: 'smooth'})}>view map</button>
+                    </div>
+                    <p className="events-address-label">Refund Policy: </p>
+                    <p  className="events-address-para">Contact the organizer to request a refund. Adventure Time's fee is nonrefundable. </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BottomEventInfo
--- react-app/src/components/Pages/event/Event.js
+++ react-app/src/components/Pages/event/Event.js
@@ -8,14 +8,14 @@ import * as actionfollowers from '../../../store/follower';
 import { all_categories } from '../../../store/category';
 import { all_venues } from '../../../store/venue';
 
-
 import MapPanel from '../../MapPanel/MapPanel'
+import TicketPanel from '../../TicketPanel/TicketPanel';
+import EditForm from '../../EditForm/EditForm';
 
 import moment from 'moment';
 
 import './Event.css'
-import TicketPanel from '../../TicketPanel/TicketPanel';
-import EditForm from '../../EditForm/EditForm';
+import BottomEventInfo from '../../BottomEventInfo/BottomEventInfo';
 
 const EachEvent = () => {
   const eventId = useParams()
@@ -185,7 +185,7 @@ const editthisevent =  async (e) => {
           <div className='event-page'>
             <div className='event-page-topcard'>
               <div className='event-page-img-container'>
-                <img alt='' className='event-page-img'src={event?.events[0]?.image}/>
+                <img alt={event?.events[0]?.image} className='event-page-img'src={event?.events[0]?.image}/>
               </div>
               <div className="event-page-card">
                 <div>
@@ -203,56 +203,7 @@ const editthisevent =  async (e) => {
             <div className="purchase-tix-bar">
                 { event?.events[0]?.host_id !== user.id ? <button type='button' onClick={() => (setPanel(!panel))} className='ticket-button'>Tickets</button> : null }
             </div>
-            <div className='event-page-bot-grid'>
-              <div className='events-main-cont-container'>
-                <div className='events-main-cont'>
-                  <div class='events-description-text'>
-                    <p>
-                      {moment(event?.events[0]?.start_time).format('MMM do')}
-                      - SINGLE DAY PASS - One (1) general admission RSVP to attend&nbsp;
-                    {event?.events[0]?.name} in {event?.events[0]?.venue?.city},&nbsp;
-                    {event?.events[0]?.venue?.state}
-                    </p>
-                    <h2 className="events-description-label">About this event</h2>
-                    <p className="events-description-innertext">{event?.events[0]?.description}</p>
-                    <div className="tags">Tags</div>
-                  </div>
-                </div>
-              </div>
-              <div className='side-panel-container'>
-                <div className='event-page-sidepanel'>
-                    <div className="events-info-label">
-                      <p>Capacity:</p>
-                    </div>
-                    <div className='events-address-para'>
-                      <p>{event?.events[0]?.capacity}</p>
-                    </div>
-                    <div className="events-info-label">
-                      <p>Category:</p>
-                    </div>
-                    <div className='events-address-para'>
-                      <p>{event?.events[0]?.category?.type}</p>
-                    </div>
-                    <p>Date and time</p>
-                    <p>Starts: </p>
-                    <p className='startend-times'>{moment(event?.events[0]?.start_time).add(5, 'hours').format('ddd[,] MMMM Do [,] YYYY [at] h:mm A')}</p>
-                    <p>Ends:</p>
-                    <p className='startend-times'>{moment(event?.events[0]?.end_time).add(5, 'hours').format('dddd[,] MMMM Do[,] YYYY [at] h:mm A')}</p>
-
-                    <p className="events-address-label">Location:</p>
-                    <div className='events-page-location'>
-                      <p className="events-address-para">{event?.events[0]?.venue?.name}</p>
-                      <p className="events-address-para">{event?.events[0]?.venue?.address}</p>
-                      <p className="events-address-para">{event?.events[0]?.venue?.city}, {event?.events[0]?.venue?.state}</p>
-                      <p className="events-address-para">{event?.events[0]?.venue?.zip_code}</p>
-                      <p className="events-address-para">Lat: {event?.events[0]?.venue?.latitude} Lon: {event?.events[0]?.venue?.longitude} </p>
-                      <button className='view-map-button' onClick={() => document.querySelector('.the-real-map').scrollIntoView({behavior: 'smooth'})}>view map</button>
-                    </div>
-                    <p className="events-address-label">Refund Policy: </p>
-                    <p  className="events-address-para">Contact the organizer to request a refund. Adventure Time's fee is nonrefundable. </p>
-                </div>
-              </div>
-            </div>
+            <BottomEventInfo event={event}/>
           </div>
           {/* {// ===========================================insert===========================================================================} */}
             <MapPanel event = {event} user={user} follower={follower} unfollow={unfollow} follow={follow}/>
