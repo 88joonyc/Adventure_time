4b55cc867aa3389c8e50255b872fe7233d82b4bf
Paul Chang <pchang@trellis.co>
2023-03-15 19:24:02 -0400

    migration

--- app/config.py
+++ app/config.py
@@ -11,3 +11,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+
+
+# postgres://adventure_time_database_user:eaC9eS2OsBLVDL8FGmiHEKmd8Kdm1Bq0@dpg-cg8jit64dad531sb44ag-a.oregon-postgres.render.com/adventure_time_database
